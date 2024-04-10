a521c1854d906d3279439d005d0b5a792246e02a
Alcides Neto <aneto@padtec.com.br>
2024-04-10 17:16:15 -0300

    Implementação da seleção de modelo e aprimoramento dos logs
    Descrição:
    
    Nova funcionalidade: Possibilidade de escolher o modelo de transcrição a ser utilizado, permitindo maior flexibilidade e controle para o usuário.
    Melhorias na exibição dos logs: Apresentação mais clara e concisa do progresso da execução do código, facilitando a identificação de erros e problemas.
    
    Detalhes técnicos:
    
    Seleção do modelo: Implementado um menu numerado para facilitar a escolha do modelo desejado pelo usuário.
    
    Melhorias nos logs:
    Separação das mensagens de log.
    Melhoria na formatação das mensagens de log.
    
    Exemplos de uso:
    
    O usuário pode escolher um modelo mais leve para transcrições rápidas de baixa qualidade.
    O usuário pode escolher um modelo mais preciso para transcrições importantes que exigem alta qualidade.

--- main.py
+++ main.py
@@ -16,47 +16,54 @@ def convert_to_wav(input_file):
 
     return output_file
 
-def transcribe_audio(input_file, result_dir):
-    # Definindo o nome do arquivo de saída para a transcrição
+def transcribe_audio(input_file, result_dir, model_choice):
+    model_mapping = {
+        1: "ggml-small.bin",
+        2: "ggml-tiny.bin",
+        3: "ggml-base.bin",
+        4: "ggml-large-v3.bin",
+        5: "ggml-medium.bin"
+    }
+    modelo = model_mapping.get(model_choice, "ggml-small.bin")
+    print("#####################################################################################################################\n")
+    print(f"Modelo selecionado: {modelo}\n")
+    print("#####################################################################################################################\n")
     output_file = os.path.join(result_dir, os.path.basename(input_file).replace(".wav", ""))
-    
-    """
-    ggml-small.bin: Este é provavelmente um modelo pequeno, destinado a tarefas simples de transcrição de áudio para texto. Pode ser útil em situações onde a precisão não é crítica e a velocidade de processamento é mais importante. Pode ser usado em dispositivos com recursos limitados ou quando a latência é uma preocupação.
-    ggml-tiny.bin: Este modelo é ainda menor que o ggml-small.bin, e provavelmente é otimizado para dispositivos com recursos muito limitados ou para transcrições muito simples e rápidas. No entanto, a precisão pode ser comprometida em comparação com modelos maiores.
-    ggml-base.bin: Este é um modelo base que oferece um equilíbrio entre precisão e velocidade. É adequado para uso geral em uma variedade de situações e pode ser uma escolha segura se você não estiver certo sobre qual modelo escolher.
-    ggml-large-v3.bin: Este é um modelo grande que provavelmente oferece a melhor precisão, mas pode exigir mais recursos computacionais e tempo de processamento. É ideal para transcrições que exigem alta precisão, como em contextos profissionais ou acadêmicos, onde a qualidade é primordial.
-    ggml-medium.bin: Este modelo provavelmente fica entre o ggml-base.bin e o ggml-large-v3.bin em termos de precisão e velocidade. Pode ser uma boa escolha se você precisar de uma precisão razoável sem comprometer muito a velocidade de processamento.
-    """
-    # Comando para executar a transcrição
-    command = f"C:\\whisper\\main.exe -f \"{input_file}\" -l portuguese -m C:\\whisper\\ggml-medium.bin --output-txt --output-file \"{output_file}\""
-    
-    # Executando o comando
+    command = f"C:\\whisper\\main.exe -f \"{input_file}\" -l portuguese -m C:\\whisper\\{modelo} --output-txt --output-file \"{output_file}\""
     subprocess.run(command, shell=True)
 
     return output_file
 
 def main():
-    # Solicita ao usuário que insira o caminho do arquivo de áudio
+    print("#####################################################################################################################")
+    print("################################## Modelos para transcrição #########################################################\n")
+    print("1. ggml-small.bin: Ideal para tarefas simples e rápidas, onde a precisão não é crucial. Perfeito para dispositivos com recursos limitados.\n")
+    print("2. ggml-tiny.bin: O menor modelo disponível, ideal para dispositivos com recursos muito limitados. A precisão pode ser menor que outros modelos.\n")
+    print("3. ggml-base.bin: Equilíbrio entre precisão e velocidade. Adequado para uso geral em diversas situações. Uma boa escolha se você não tem certeza qual modelo usar.\n")
+    print("4. ggml-large-v3.bin: Maior modelo, com a melhor precisão. Exige mais recursos computacionais e tempo de processamento. Ideal para transcrições que exigem alta qualidade, como em contextos profissionais ou acadêmicos.\n")
+    print("5. ggml-medium.bin: Intermediário entre o ggml-base.bin e o ggml-large-v3.bin em termos de precisão e velocidade. Uma boa escolha quando você precisa de boa precisão sem comprometer muito a velocidade.\n")
+    print("#####################################################################################################################\n")
+    model_choice = int(input("Digite o número correspondente ao modelo desejado: "))
+    print("#####################################################################################################################\n")
+    print("################################## Informe o caminho do arquivo de áudio ############################################\n")
     input_file = input("Insira o caminho do arquivo de áudio: ")
-    
-    # Verifica se o arquivo existe
+    print("#####################################################################################################################\n")
     if not os.path.exists(input_file):
         print("O arquivo especificado não existe.")
         return
 
-    # Define o diretório de saída para os arquivos transcritos
     result_dir = "C:\\whisper\\WhisperTranscribe\\result"
     os.makedirs(result_dir, exist_ok=True)
     
-    # Convertendo o arquivo de áudio para WAV
     wav_file = convert_to_wav(input_file)
-    print(f"Arquivo convertido para WAV: {wav_file}")
-    
-    # Transcrevendo o arquivo de áudio convertido
-    transcribed_file = transcribe_audio(wav_file, result_dir)
-    print(f"Transcrição completa. Arquivo de saída: {transcribed_file}")
+    print("################################## Conversão de áudio ################################################################\n")
+    print(f"Arquivo convertido para WAV: {wav_file}\n")
+    print("#####################################################################################################################\n")
 
-    # Exclui o arquivo WAV convertido após a transcrição
+    transcribed_file = transcribe_audio(wav_file, result_dir, model_choice)
+    print("################################## Transcrição de áudio #############################################################\n")
+    print(f"Transcrição completa. Arquivo de saída: {transcribed_file}\n")
+    print("#####################################################################################################################\n")
     os.remove(wav_file)
 
 if __name__ == "__main__":
